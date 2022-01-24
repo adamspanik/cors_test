d9a9e698fe85cd19669473e80440cd2af80522a7
Adam Spanik <67162200+adamspanik@users.noreply.github.com>
2022-01-24 14:32:09 +0100

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ function main()
     console.log("adam-spanik-cors-test");
     $.ajax
     ({
-        url: "http://www.cgistodulky.cz/csr/",
-        headers: {"Access-Control-Allow-Origin": "*"},
-    // http://localhost:8080/csrcovid-1.0.0/api/dev
+        url: "https://www.cgistodulky.cz/csr/",
+    // https://localhost:8080/csrcovid-1.0.0/api/dev
     // https://www.cgistodulky.cz/csr/
     // https://www.cgistodulky.cz/csr/api/dev
         success: function(data) 
